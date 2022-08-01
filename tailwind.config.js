8f3eba6fb6821db9e9821d884875efe1eba4de11
itisbunmioke <itisbunmioke@gmail.com>
2022-08-01 01:48:29 -0600

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "144px",
+    },
+    extend: {
+      colors: {
+        strongCyan: "hsl(171, 66%, 44%)",
+        lightBlue: "hsl(233, 100%, 69%)",
+        darkGrayishBlue: "hsl(210, 10%, 33%)",
+        grayishBlue: "hsl(201, 11%, 66%)",
+        atomicTangerine: "hsl(15, 100%, 72%)",
+      },
+      fontFamily: {
+        sans: ["Bai Jamjuree", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
